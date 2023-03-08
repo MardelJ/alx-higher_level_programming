f56e02d63025c418b1e5a45b2c24964e395bc006
Mardel J. Juma <Mardel.mjj@gmail.com>
2023-03-08 11:28:06 -0800

    Fizzbuzzinho

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,9 +3,9 @@ def fizzbuzz():
     for number in range(1, 100):
         if number % 3 == 0 and number % 5 == 0:
             print("FizzBuzz", end="")
-        elif number % 3 == 0:
-            print("Fizz", end="")
         elif number % 5 == 0:
             print("Buzz", end="")
+        elif number % 3 == 0:
+            print("Fizz", end="")
         else:
             print(f"{number} ", end="")
