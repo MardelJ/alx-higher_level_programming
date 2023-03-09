275e407df5a7eae145b87dd864aa78bbbf0bf58c
Mardel J. Juma <Mardel.mjj@gmail.com>
2023-03-09 04:24:51 -0800

    From add imported

---
+++ 0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Program to import add and use to calculate"""
+
+if __name__ = "__main__"
+
+from add_0.py import add
+a = 1
+b = 2
+print("{} + {} = {}".format(a, b, add(a,b))
