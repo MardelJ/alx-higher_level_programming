d83a5a69ad1147f2cdc47b2b07637efdbf8e32d8
Mardel J. Juma <Mardel.mjj@gmail.com>
2023-03-09 04:29:01 -0800

    Added

--- 0-add.py
+++ 0-add.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-""" Program to import add and use to calculate"""
+if __name__ = "__main__":
+    """ Program to import add and use to calculate"""
 
-if __name__ = "__main__"
-
-from add_0.py import add
+from add_0 import add
 a = 1
 b = 2
 print("{} + {} = {}".format(a, b, add(a,b))
---
+++ README.md
@@ -0,0 +1,1 @@
+Diving into High Levele Programmin
